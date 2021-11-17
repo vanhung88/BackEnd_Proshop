119dc9554ee9bde2fd526e2c7b543ac0c9095098
vanhung88 <88levanhung@gmail.com>
2021-11-17 21:16:23 +0700

    first

---
+++ data/products.js
@@ -0,0 +1,99 @@
+const products = [
+  {
+    name: "Sleeve Longline Sweater",
+    image1: "https://i.ibb.co/1nQ8BtN/product4b.webp",
+    image2: "https://i.ibb.co/jwjpkdt/product4a.webp",
+    description:
+      "FIGHTING FIT Go sporty this summer with this vintage navy and white striped v-neck t-shirt from the Abercrombie & Fitch. Perfect for...",
+    category: "Sweater",
+    price: 29.99,
+    countInStock: {
+      S: 0,
+      M: 13,
+      L: 200,
+      XL: 0,
+    },
+  },
+  {
+    name: "Solid Button Shorts",
+    image2: "https://i.ibb.co/F4t9hwz/product6b.webp",
+    image1: "https://i.ibb.co/vk3Bsy0/product6b.webp",
+
+    description:
+      "Creativit We buy with our eyes, and then come back for the taste. At Eric Kayser, creativity is central to the development...",
+    category: "Short",
+    price: 19.99,
+    countInStock: {
+      S: 0,
+      M: 13,
+      L: 200,
+      XL: 0,
+    },
+  },
+  {
+    name: "Tie Front Tropical Top",
+    image2: "https://i.ibb.co/4gKfmMg/product3b.webp",
+    image1: "https://i.ibb.co/HzDtvsp/product3a.webp",
+
+    description:
+      "Premium Materials Blandit parturient adipiscing faucibus fringilla vestibulum ultrices integer dolor parturient parturient at porta platea inceptos. Habitant dui ut fringilla eleifend..",
+    category: "Top",
+    price: 19.99,
+    countInStock: {
+      S: 0,
+      M: 13,
+      L: 100,
+      XL: 0,
+    },
+  },
+  {
+    name: "Tie Front Tropical Top",
+    image2: "https://i.ibb.co/T0TyJrq/product2b.webp",
+    image1: "https://i.ibb.co/rvx43b8/product2a.webp",
+
+    description:
+      "FIGHTING FIT Go sporty this summer with this vintage navy and white striped v-neck t-shirt from the Abercrombie & Fitch. Perfect for...",
+    category: "Sport",
+    price: 37.99,
+    countInStock: {
+      S: 0,
+      M: 13,
+      L: 100,
+      XL: 0,
+    },
+  },
+  {
+    name: "Ripped Distressed Top",
+    image2: "https://i.ibb.co/PzjH829/product1a.webp",
+    image1: "https://i.ibb.co/VqQN7X1/product1b.webp",
+
+    description:
+      "Premium Materials Blandit parturient adipiscing faucibus fringilla vestibulum ultrices integer dolor parturient parturient at porta platea inceptos. Habitant dui ut fringilla eleifend... ",
+    category: "Sport",
+    price: 22.99,
+    countInStock: {
+      S: 0,
+      M: 13,
+      L: 100,
+      XL: 0,
+    },
+  },
+  {
+    name: "Shoulder Heart Sweater",
+    image2: "https://i.ibb.co/c6wStzG/product5c.jpg",
+    image1: "https://i.ibb.co/hcCfXXf/product5b.webp",
+
+    description:
+      "Premium Materials Blandit parturient adipiscing faucibus fringilla vestibulum ultrices integer dolor parturient parturient at porta platea inceptos. Habitant dui ut fringilla eleifend... ",
+    category: "Sweater",
+    price: 42.5,
+    countInStock: {
+      S: 0,
+      M: 13,
+      L: 100,
+      XL: 0,
+    },
+  },
+];
+
+export default products;
